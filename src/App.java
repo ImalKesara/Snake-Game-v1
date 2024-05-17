a5cdfa84d074c3ac12ffb7d7839b26f3b73e69ab
LoneWolf <imalkesara2001@gmail.com>
2024-05-17 13:27:58 +0530

    First commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception{
+        int BoardWidth = 600;
+        int BoardHeight = BoardWidth;
+
+        JFrame jFrame = new JFrame("Snake Game");
+        jFrame.setVisible(true);
+
+
+    }
+}
